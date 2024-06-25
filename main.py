e26be86a776dc8d8d1531deb0296fcd7dd54741c
JB <149854676+jb-gy@users.noreply.github.com>
2024-06-25 18:30:08 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# #alternative
-# def reverse_only_letters(s):
-#   char_positions = []
-#   others = []
-#   final_str = []
 
-#   for index, val in enumerate(s):
-#     if val.isalpha():
-#       char_positions.append(index)
-#     else:
-#       others.append(index)
+def reverse_only_letters(s):
+  char_positions = []
+  others = []
+  final_str = []
 
-#   flipped = char_positions[::-1]
-#   print(flipped)
-#   print(others)
+  for index, val in enumerate(s):
+    if val.isalpha():
+      char_positions.append(index)
+    else:
+      others.append(index)
+
+  flipped = char_positions[::-1]
+  print(flipped)
+  print(others)
   
  
 
@@ -20,7 +20,7 @@
 
 
 
-# s = "a-bC-dEf-ghIj"
-# reversed_s = reverse_only_letters(s)
-# #print(reversed_s)
+s = "a-bC-dEf-ghIj"
+reversed_s = reverse_only_letters(s)
+
     
